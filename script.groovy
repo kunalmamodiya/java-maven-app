b2c3c4576c73e7aadeb39c26cb1d39875642a6f4
kunal.mamodiya <kunal.mamodiya@thepsi.com>
2025-02-15 16:10:45 +0530

    changes in groovy script

--- script.groovy
+++ script.groovy
@@ -5,11 +5,6 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'podman build -t kunalmamodiya/demo-app:jma-2.0 .'
-        sh "echo $PASS | podman login -u $USER --password-stdin"
-        sh 'podman push kunalmamodiya/demo-app:jma-2.0'
-    }
 } 
 
 def deployApp() {
